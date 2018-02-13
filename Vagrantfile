e299df525c15c43a05e11fde4258d6f9c88ba0e5
ksichien <ksichien@posteo.net>
2018-02-13 22:44:25 +0100

    added nodejs to web role

--- Vagrantfile
+++ Vagrantfile
@@ -72,7 +72,7 @@ Vagrant.configure(2) do |config|
   config.vm.provision "ansible" do |ansible|
     ansible.playbook = "vagrant.yml"
     ansible.inventory_path = "hosts"
-    ansible.verbose = "-v"
+    ansible.verbose = ""
   end
 
 end
--- roles/web/templates/devise.rb
+++ roles/web/templates/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
     # confirmation, reset password and unlock tokens in the database.
     # Devise will use the `secret_key_base` as its `secret_key`
     # by default. You can change it below and use your own secret key.
-    config.secret_key = '{{ application.devise_secret_key }}'
+    config.secret_key = '{{ application.ruby.devise_secret_key }}'
 
     # ==> Mailer Configuration
     # Configure the e-mail address which will be shown in Devise::Mailer,
